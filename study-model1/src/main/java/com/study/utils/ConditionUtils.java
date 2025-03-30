563573720919d05bf0e2622c1ae738ae5ac66943
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2025-03-30 23:23:19 +0900

    feat : Condition Utils

---
+++ study-model1/src/main/java/com/study/utils/ConditionUtils.java
@@ -0,0 +1,31 @@
+package com.study.utils;
+
+import com.study.condition.SearchCondition;
+
+/**
+ * 조건에 대한 Utils 클래스
+ */
+public class ConditionUtils {
+
+    /**
+     * 검색조건이 없는 경우와 검색조건이 있는 경우에 맞는 SearchCondition 반환
+     *
+     * @param startDate  처음 검색 기간
+     * @param endDate    끝 검색 기간
+     * @param categoryId 카테고리
+     * @param searchText 검색어
+     * @param pageNum    페이지
+     * @return 검색조건 객체
+     */
+    public static SearchCondition parameterToSearchCondition(String startDate, String endDate, String categoryId, String searchText, String pageNum) {
+        boolean isInit = startDate == null || endDate == null || categoryId == null || searchText == null;
+        if (isInit) {
+            return new SearchCondition();
+        } else {
+            if (pageNum == null) {
+                pageNum = "1";
+            }
+            return new SearchCondition(startDate, endDate, categoryId, searchText, pageNum);
+        }
+    }
+}
