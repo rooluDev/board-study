1ce9c318faa249ea496cc6014aae4f3df420a08d
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2025-04-07 03:02:33 +0900

    refactor : Dto Builder

--- study-model2/src/main/java/com/study/command/AddProcCommand.java
+++ study-model2/src/main/java/com/study/command/AddProcCommand.java
@@ -4,127 +4,121 @@ import com.study.DAO.BoardDAO;
 import com.study.DAO.FileDAO;
 import com.study.DTO.BoardDTO;
 import com.study.DTO.FileDTO;
-import com.study.exception.CategoryException;
-import com.study.exception.ContentException;
-import com.study.exception.TitleException;
-import com.study.exception.UserNameException;
 import com.study.properties.FileProperties;
-import com.study.utils.StringUtils;
 import com.study.validate.BoardFormValidate;
-import java.io.File;
-import java.io.IOException;
+
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.fileupload.FileItem;
-import org.apache.commons.fileupload.FileUploadException;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.apache.commons.io.FilenameUtils;
 
+/**
+ * 게시판 추가 로직 프로세스
+ */
 public class AddProcCommand implements Command {
 
-    public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        boolean isMultipart = ServletFileUpload.isMultipartContent(request);
+        if (!isMultipart) {
+            response.sendRedirect("/error.jsp");
+        }
+
         FileProperties fileProperties = new FileProperties();
         Map<String, String> propertiesMap = fileProperties.getProperties();
-        String path = (String)propertiesMap.get("filePath");
-        String characterSet = (String)propertiesMap.get("characterSet");
-        request.setCharacterEncoding(characterSet);
-        response.setContentType("text/html; charset=UTF-8");
-        BoardDTO board = new BoardDTO();
-        List<FileDTO> fileList = new ArrayList();
-        BoardFormValidate boardFormValidate = new BoardFormValidate();
+        String path = propertiesMap.get("filePath");
+        String characterSet = propertiesMap.get("characterSet");
+
         DiskFileItemFactory diskFileItemFactory = new DiskFileItemFactory();
-        diskFileItemFactory.setRepository(new File(path));
-        diskFileItemFactory.setDefaultCharset(characterSet);
-        ServletFileUpload fileUpload = new ServletFileUpload(diskFileItemFactory);
+        ServletFileUpload servletFileUpload = new ServletFileUpload(diskFileItemFactory);
+        servletFileUpload.setHeaderEncoding("UTF-8");
+        servletFileUpload.setSizeMax(2 * 1024 * 1024);
 
-        try {
-            List<FileItem> fileItemList = fileUpload.parseRequest(request);
+        List<FileItem> fileItemList = servletFileUpload.parseRequest(request);
 
-            for(FileItem item : fileItemList) {
-                String fieldName;
-                if (!item.isFormField()) {
-                    fieldName = item.getName();
-                    if (!StringUtils.isNull(fieldName)) {
-                        Long size = item.getSize();
-                        String physicalName = UUID.randomUUID().toString();
-                        String extension = FilenameUtils.getExtension(fieldName);
-                        String separator = File.separator;
-                        FileDTO file = new FileDTO();
-                        file.setOriginalName(fieldName);
-                        file.setPhysicalName(physicalName);
-                        file.setFilePath((String)propertiesMap.get("filePath"));
-                        file.setExtension(extension);
-                        file.setSize(size.intValue());
-                        String filePath = path + separator + physicalName + "." + extension;
-                        File uploadedFile = new File(filePath);
-                        item.write(uploadedFile);
-                        fileList.add(file);
-                    }
-                } else {
-                    fieldName = item.getFieldName();
-                    String fieldValue = item.getString(characterSet);
-                    if (fieldName.equals("category")) {
-                        if (!boardFormValidate.validateCategory(Integer.parseInt(fieldValue))) {
-                            throw new CategoryException("category value error");
-                        }
+        int categoryId = 0;
+        String userName = null;
+        String password = null;
+        String passwordRe = null;
+        String title = null;
+        String content = null;
+        List<FileDTO> fileList = new ArrayList<>();
 
-                        board.setCategoryId(Integer.parseInt(fieldValue));
-                    } else if (fieldName.equals("userName")) {
-                        if (!boardFormValidate.validateUserName(fieldName)) {
-                            throw new UserNameException("userName value error");
-                        }
+        for (FileItem item : fileItemList) {
+            String fieldName = item.getFieldName();
+            String fieldValue = item.getString(characterSet);
+            if (item.isFormField()) {
+                if ("categoryId".equals(fieldName)) {
+                    categoryId = Integer.parseInt(fieldValue);
+                } else if ("userName".equals(fieldName)) {
+                    userName = fieldValue;
+                } else if ("password".equals(fieldName)) {
+                    password = fieldValue;
+                } else if ("passwordRe".equals(fieldName)) {
+                    passwordRe = fieldValue;
+                } else if ("title".equals(fieldName)) {
+                    title = fieldValue;
+                } else if ("content".equals(fieldName)) {
+                    content = fieldValue;
+                }
+            } else {
+                boolean fileNullCheck = item == null || item.getName().isEmpty();
+                if (!fileNullCheck) {
+                    int size = (int) item.getSize();
+                    String physicalName = UUID.randomUUID().toString();
+                    String fileName = item.getName();
+                    String extension = FilenameUtils.getExtension(fileName);
 
-                        board.setUserName(fieldValue);
-                    } else if (fieldName.equals("password")) {
-                        if (!boardFormValidate.validatePassword(fieldName)) {
-                        }
+                    FileDTO file = FileDTO.builder()
+                            .physicalName(physicalName)
+                            .originalName(fileName)
+                            .extension(extension)
+                            .size(size)
+                            .filePath(path)
+                            .build();
 
-                        board.setPassword(fieldValue);
-                    } else if (fieldName.equals("title")) {
-                        if (!boardFormValidate.validateTitle(fieldName)) {
-                            throw new TitleException("title value error");
-                        }
+                    fileList.add(file);
 
-                        board.setTitle(fieldValue);
-                    } else if (fieldName.equals("content")) {
-                        if (!boardFormValidate.validateContent(fieldName)) {
-                            throw new ContentException("content value error");
-                        }
-
-                        board.setContent(fieldValue);
-                    }
+                    String filePath = path + "/" + physicalName + "." + extension;
+                    java.io.File uploadedFile = new java.io.File(filePath);
+                    item.write(uploadedFile);
                 }
             }
-        } catch (FileUploadException var25) {
-            var25.printStackTrace();
-        } catch (IllegalStateException var26) {
-            response.sendRedirect("/board?command=error");
-        } catch (Exception var27) {
-            throw new RuntimeException(var27);
         }
+
+        BoardDTO board = BoardDTO.builder()
+                .userName(userName)
+                .categoryId(categoryId)
+                .password(password)
+                .passwordRe(passwordRe)
+                .title(title)
+                .content(content)
+                .build();
+
+        if (!BoardFormValidate.validateBoardForPost(board)) {
+            response.sendRedirect("/error.jsp");
+        }
+
 
         BoardDAO boardDAO = new BoardDAO();
         int boardId = boardDAO.insertBoard(board);
         FileDAO fileDAO = new FileDAO();
-        Iterator var31 = fileList.iterator();
-
-        while(var31.hasNext()) {
-            FileDTO uploadFile = (FileDTO)var31.next();
-            System.out.println(uploadFile.getOriginalName());
-            System.out.println(uploadFile.getExtension());
-            uploadFile.setBoardId(boardId);
-            fileDAO.insertFile(uploadFile);
-        }
+        fileList.forEach(file -> {
+            file.setBoardId(boardId);
+            try {
+                fileDAO.insertFile(file);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
 
         response.sendRedirect("/board?command=list");
     }
-}
 
+}
