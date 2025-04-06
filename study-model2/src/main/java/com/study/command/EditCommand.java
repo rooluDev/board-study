38c52bb6fefbc8d95b7170b60f97a84a4a2c38dd
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2025-04-07 03:06:45 +0900

    feat : 검색조건 유지

--- study-model2/src/main/java/com/study/command/EditCommand.java
+++ study-model2/src/main/java/com/study/command/EditCommand.java
@@ -1,33 +1,42 @@
 package com.study.command;
 
 import com.study.DAO.BoardDAO;
-import com.study.DAO.CategoryDAO;
 import com.study.DAO.FileDAO;
 import com.study.DTO.BoardDTO;
 import com.study.DTO.FileDTO;
-import java.io.IOException;
 import java.util.List;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+/**
+ * 수정 페이지로 이동
+ */
 public class EditCommand implements Command {
 
-    public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
         String path = "/WEB-INF/views/edit.jsp";
+
         int boardId = Integer.parseInt(request.getParameter("boardId"));
-        int pageNum = Integer.parseInt(request.getParameter("pageNum"));
+        String pageNum = request.getParameter("pageNum");
+        String startDate = request.getParameter("startDate");
+        String endDate = request.getParameter("endDate");
+        String categoryId = request.getParameter("categoryId");
+        String searchText = request.getParameter("searchText");
+
+
         BoardDAO boardDAO = new BoardDAO();
-        CategoryDAO categoryDAO = new CategoryDAO();
         FileDAO fileDAO = new FileDAO();
-        BoardDTO board = boardDAO.findById(boardId);
-        String categoryName = categoryDAO.findById(board.getCategoryId());
-        List<FileDTO> fileList = fileDAO.findByBoardId(boardId);
+        BoardDTO board = boardDAO.selectBoardById(boardId);
+        List<FileDTO> fileList = fileDAO.selectFileListByBoardId(boardId);
+
         request.setAttribute("board", board);
         request.setAttribute("fileList", fileList);
-        request.setAttribute("categoryName", categoryName);
+        request.setAttribute("categoryId", categoryId);
         request.setAttribute("pageNum", pageNum);
-        request.setAttribute("boardId", boardId);
+        request.setAttribute("startDate", startDate);
+        request.setAttribute("endDate", endDate);
+        request.setAttribute("searchText", searchText);
+
         request.getRequestDispatcher(path).forward(request, response);
     }
 }
