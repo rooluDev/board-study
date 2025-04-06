2f1d7aa361bdbb58a62c41d7d9a4ccc86736dc0e
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2025-04-07 03:05:27 +0900

    fix : dto Builder 패턴 사용

--- study-model2/src/main/java/com/study/command/CommentProc.java
+++ study-model2/src/main/java/com/study/command/CommentProc.java
@@ -2,22 +2,33 @@ package com.study.command;
 
 import com.study.DAO.CommentDAO;
 import com.study.DTO.CommentDTO;
-import java.io.IOException;
-import javax.servlet.ServletException;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+/**
+ * 댓글 추가 프로세스
+ */
 public class CommentProc implements Command {
 
-    public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
         String comment = request.getParameter("comment");
         int boardId = Integer.parseInt(request.getParameter("boardId"));
         int pageNum = Integer.parseInt(request.getParameter("pageNum"));
-        CommentDTO commentDTO = new CommentDTO();
-        commentDTO.setBoardId(boardId);
-        commentDTO.setComment(comment);
+        String startDate = request.getParameter("startDate");
+        String endDate = request.getParameter("endDate");
+        String categoryId = request.getParameter("categoryId");
+        String searchText = request.getParameter("searchText");
+
+
+        CommentDTO commentDTO = CommentDTO.builder()
+                .boardId(boardId)
+                .comment(comment)
+                .build();
+
         CommentDAO commentDAO = new CommentDAO();
         commentDAO.insertComment(commentDTO);
-        response.sendRedirect("/board?command=view&boardId=" + boardId + "&pageNum=" + pageNum);
+
+        response.sendRedirect("/board?command=view&boardId=" + boardId + "&pageNum=" + pageNum + "&startDate=" + startDate + "&endDate=" + endDate + "&categoryId=" + categoryId + "&searchText=" + searchText);
     }
 }
