c165dc32e870168a8366555388518e8691be936e
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2025-04-07 03:06:03 +0900

    fix : delete 로직 수정

--- study-model2/src/main/java/com/study/command/DeleteCommand.java
+++ study-model2/src/main/java/com/study/command/DeleteCommand.java
@@ -1,18 +1,28 @@
 package com.study.command;
 
 import com.study.DAO.BoardDAO;
-import java.io.IOException;
-import javax.servlet.ServletException;
+import com.study.DAO.CommentDAO;
+import com.study.DAO.FileDAO;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+/**
+ * 게시판 삭제 프로세스
+ */
 public class DeleteCommand implements Command {
 
-    public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        int pageNum = Integer.parseInt(request.getParameter("pageNum"));
+    public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
         int boardId = Integer.parseInt(request.getParameter("boardId"));
+
         BoardDAO boardDAO = new BoardDAO();
+        CommentDAO commentDAO = new CommentDAO();
+        FileDAO fileDAO = new FileDAO();
+
+        commentDAO.deleteCommentsByBoardId(boardId);
+        fileDAO.deleteByBoardId(boardId);
         boardDAO.deleteById(boardId);
-        response.sendRedirect("/board?command=list&pageNum=" + pageNum);
+
+        response.sendRedirect("/board?command=list");
     }
 }
