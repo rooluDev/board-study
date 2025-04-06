d0f4624da8d1cddd3f5882bee990a38dab466a45
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2025-04-07 03:01:48 +0900

    refactor : 검색조건 유지

--- study-model2/src/main/java/com/study/command/AddCommand.java
+++ study-model2/src/main/java/com/study/command/AddCommand.java
@@ -1,16 +1,32 @@
 package com.study.command;
 
 import com.study.DAO.CategoryDAO;
-import java.io.IOException;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+/**
+ * 게시판 추가 페이지로 이동
+ */
 public class AddCommand implements Command {
-    public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
         String path = "/WEB-INF/views/add.jsp";
+
+        String pageNum = request.getParameter("pageNum");
+        String startDate = request.getParameter("startDate");
+        String endDate = request.getParameter("endDate");
+        String categoryId = request.getParameter("categoryId");
+        String searchText = request.getParameter("searchText");
+
         CategoryDAO categoryDAO = new CategoryDAO();
+
         request.setAttribute("categoryList", categoryDAO.getCategoryList());
+        request.setAttribute("pageNum", pageNum);
+        request.setAttribute("startDate", startDate);
+        request.setAttribute("endDate", endDate);
+        request.setAttribute("categoryId", categoryId);
+        request.setAttribute("searchText", searchText);
+
         request.getRequestDispatcher(path).forward(request, response);
     }
 }
