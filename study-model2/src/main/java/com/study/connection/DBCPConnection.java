363c4dbb7a8cb6dc848e18aaf19448a772df7938
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2025-04-07 03:05:44 +0900

    docs : 주석 추가

--- study-model2/src/main/java/com/study/connection/DBCPConnection.java
+++ study-model2/src/main/java/com/study/connection/DBCPConnection.java
@@ -9,47 +9,60 @@ import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
 
+/**
+ * 커넥션 풀
+ */
 public class DBCPConnection {
 
+    /**
+     * connection 연결
+     *
+     * @return Connection
+     */
     public Connection getConnection() {
         Connection connection = null;
         String jndiName = "jdbc/mysql";
 
         try {
-            Context initContext = (Context)(new InitialContext()).lookup("java:comp/env");
-            DataSource dataSource = (DataSource)initContext.lookup(jndiName);
+            Context initContext = (Context) (new InitialContext()).lookup("java:comp/env");
+            DataSource dataSource = (DataSource) initContext.lookup(jndiName);
             connection = dataSource.getConnection();
-        } catch (SQLException var5) {
-            var5.printStackTrace();
-        } catch (NamingException var6) {
-            var6.printStackTrace();
+        } catch (SQLException | NamingException sqlException) {
+            sqlException.printStackTrace();
         }
 
         return connection;
     }
 
+    /**
+     * 리소스 연결 해제
+     *
+     * @param connection Connection
+     * @param pstmt PreparedStatement
+     * @param resultSet ResultSet
+     */
     public void closeConnections(Connection connection, PreparedStatement pstmt, ResultSet resultSet) {
         if (resultSet != null) {
             try {
                 resultSet.close();
-            } catch (SQLException var7) {
-                var7.printStackTrace();
+            } catch (SQLException sqlException) {
+                sqlException.printStackTrace();
             }
         }
 
         if (pstmt != null) {
             try {
                 pstmt.close();
-            } catch (SQLException var6) {
-                var6.printStackTrace();
+            } catch (SQLException sqlException) {
+                sqlException.printStackTrace();
             }
         }
 
         if (connection != null) {
             try {
                 connection.close();
-            } catch (SQLException var5) {
-                var5.printStackTrace();
+            } catch (SQLException sqlException) {
+                sqlException.printStackTrace();
             }
         }
 
