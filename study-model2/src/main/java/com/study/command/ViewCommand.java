8fd63bbd6345ee3822dfb3e0d4930691505a1be7
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2025-04-07 03:08:00 +0900

    feat : 검색조건 유지

--- study-model2/src/main/java/com/study/command/ViewCommand.java
+++ study-model2/src/main/java/com/study/command/ViewCommand.java
@@ -1,45 +1,50 @@
 package com.study.command;
 
 import com.study.DAO.BoardDAO;
-import com.study.DAO.CategoryDAO;
 import com.study.DAO.CommentDAO;
 import com.study.DAO.FileDAO;
 import com.study.DTO.BoardDTO;
 import com.study.DTO.CommentDTO;
 import com.study.DTO.FileDTO;
-import java.io.IOException;
+
 import java.util.List;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+/**
+ * 게시판 보기 페이지 이동
+ */
 public class ViewCommand implements Command {
 
-    public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
         String path = "/WEB-INF/views/board.jsp";
 
+        String pageNum = request.getParameter("pageNum");
+        String startDate = request.getParameter("startDate");
+        String endDate = request.getParameter("endDate");
+        String categoryId = request.getParameter("categoryId");
+        String searchText = request.getParameter("searchText");
         int boardId = Integer.parseInt(request.getParameter("boardId"));
-        int pageNum = Integer.parseInt(request.getParameter("pageNum"));
 
         BoardDAO boardDAO = new BoardDAO();
         CommentDAO commentDAO = new CommentDAO();
         FileDAO fileDAO = new FileDAO();
-        CategoryDAO categoryDAO = new CategoryDAO();
 
-        BoardDTO board = boardDAO.findById(boardId);
+        BoardDTO board = boardDAO.selectBoardById(boardId);
         boardDAO.plusViewById(boardId);
 
-        String categoryName = categoryDAO.findById(board.getCategoryId());
+        List<CommentDTO> commentList = commentDAO.selectCommentListByBoardId(boardId);
+        List<FileDTO> fileList = fileDAO.selectFileListByBoardId(boardId);
 
-        List<CommentDTO> commentList = commentDAO.getCommentList(boardId);
-        List<FileDTO> fileList = fileDAO.findByBoardId(boardId);
-
-        request.setAttribute("boardId", boardId);
-        request.setAttribute("pageNum", pageNum);
         request.setAttribute("board", board);
-        request.setAttribute("categoryName", categoryName);
         request.setAttribute("commentList", commentList);
         request.setAttribute("fileList", fileList);
+        request.setAttribute("pageNum", pageNum);
+        request.setAttribute("categoryId", categoryId);
+        request.setAttribute("startDate", startDate);
+        request.setAttribute("endDate", endDate);
+        request.setAttribute("searchText", searchText);
+
         request.getRequestDispatcher(path).forward(request, response);
     }
 }
