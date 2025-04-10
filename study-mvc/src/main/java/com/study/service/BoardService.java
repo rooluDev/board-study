018eff2bd5fd43cfd438d68e61961fc02e3c703e
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2025-04-10 16:22:26 +0900

    docs : 주석 추가

--- study-mvc/src/main/java/com/study/service/BoardService.java
+++ study-mvc/src/main/java/com/study/service/BoardService.java
@@ -1,86 +1,97 @@
 package com.study.service;
 
+import com.study.condition.SearchCondition;
 import com.study.dto.BoardDTO;
-import com.study.condition.BoardSelectCondition;
 import com.study.mapper.BoardMapper;
-import com.study.returnType.BoardListAndCount;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * 보드 서비스
  */
 @Service
+@RequiredArgsConstructor
 public class BoardService {
+
     private final BoardMapper boardMapper;
 
-    @Autowired
-    public BoardService(BoardMapper boardMapper){
-        this.boardMapper = boardMapper;
+    /**
+     * 조건에 맞는 게시물 리스트
+     *
+     * @param searchCondition 검색조건
+     * @return 게시물 리스트
+     */
+    public List<BoardDTO> getBoardListByCondition(SearchCondition searchCondition) {
+        return boardMapper.selectBoardListByCondition(searchCondition);
     }
 
     /**
-     * 검색
-     * @param boardSelectCondition
-     * @return
+     * 조건에 맞는 게시물 수
+     *
+     * @param searchCondition 검색조건
+     * @return 게시물 수
      */
-    public BoardListAndCount search(BoardSelectCondition boardSelectCondition){
-        BoardListAndCount boardListAndCount = BoardListAndCount.builder()
-                .boardList(boardMapper.getBoardList(boardSelectCondition))
-                .rowCount(boardMapper.getBoardCount(boardSelectCondition))
-                .build();
-
-        return boardListAndCount;
-    }
-
-    /**
-     * 추가
-     * @param board
-     * @return
-     */
-    public void addBoard(BoardDTO board){
-        boardMapper.createBoard(board);
+    public int getBoardCountByCondition(SearchCondition searchCondition) {
+        return boardMapper.selectBoardCountByCondition(searchCondition);
     }
 
     /**
      * boardId로 BoardDTO 찾기
-     * @param boardId
-     * @return
+     *
+     * @param boardId pk
+     * @return 게시물
      */
-    public BoardDTO findBoard(long boardId){
-        return boardMapper.findById(boardId);
+    public BoardDTO getBoard(Long boardId) {
+        return boardMapper.selectById(boardId);
+    }
+
+    /**
+     * 추가
+     *
+     * @param board 추가할 게시물 데이터
+     */
+    public Long postBoard(BoardDTO board) {
+        boardMapper.insertBoard(board);
+        return board.getBoardId();
     }
 
     /**
      * board 수정
-     * @param board
+     *
+     * @param board 수정할 게시물 데이터
      */
-    public void modifyBoard(BoardDTO board){
+    public void editBoard(BoardDTO board) {
         boardMapper.updateBoard(board);
     }
 
     /**
-     * password 찾기
-     * @param boardId
-     * @return
+     * boardId와 입력한 pw와 일치하는 지 확인
+     *
+     * @param boardId         pk
+     * @param enteredPassword 입력한 비밀번호
+     * @return count
      */
-    public String findPassword(Long boardId){
-        return boardMapper.selectPassword(boardId);
+    public boolean findByIdAndPassword(Long boardId, String enteredPassword) {
+        return boardMapper.selectByIdAndPassword(boardId, enteredPassword) == 1;
     }
 
     /**
      * 조회수 증가
-     * @param boardId
+     *
+     * @param boardId pk
      */
-    public void increaseView(Long boardId){
+    public void increaseView(Long boardId) {
         boardMapper.updateView(boardId);
     }
 
     /**
      * 게시물 삭제
-     * @param boardId
+     *
+     * @param boardId pk
      */
-    public void deleteBoard(Long boardId){
+    public void deleteBoard(Long boardId) {
         boardMapper.deleteById(boardId);
     }
 }
