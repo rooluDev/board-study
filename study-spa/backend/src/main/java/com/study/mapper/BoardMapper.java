c8fff475178f5a46e4b1ce71b95423951519613e
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2025-05-18 22:32:38 +0900

    fix : method rename

--- study-spa/backend/src/main/java/com/study/mapper/BoardMapper.java
+++ study-spa/backend/src/main/java/com/study/mapper/BoardMapper.java
@@ -1,59 +1,80 @@
 package com.study.mapper;
 
-import com.study.condition.BoardSelectCondition;
+import com.study.condition.SearchCondition;
 import com.study.dto.BoardDto;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
-import java.util.Optional;
 
 /**
- * Board mapper
+ * BoardDto mapper
  */
 @Mapper
 public interface BoardMapper {
-    /**
-     * 검색조건과 페이지네이션에 따라 목록에 보여줄 데이터 검색
-     * @param boardSelectCondition
-     * @return
-     */
-    List<BoardDto> getBoardList(BoardSelectCondition boardSelectCondition);
 
     /**
-     * 검색조건에 따른 전체 Board rowcount 가져오기
-     * @param boardSelectCondition
-     * @return
+     * 검색조건에 따른 BoardList 가져오기
+     *
+     * @param searchCondition 검색조건
+     * @return BoardList
      */
-    int getBoardCount(BoardSelectCondition boardSelectCondition);
+    List<BoardDto> selectBoardListByCondition(SearchCondition searchCondition);
 
     /**
-     * pk로 board 데이터와 카테고리 이름 가져오기
-     * @param boardId
-     * @return
+     * 검색조건에 따른 Board rowcount 가져오기
+     *
+     * @param searchCondition 검색조건
+     * @return board count
      */
-    Optional<BoardDto> findById(Long boardId);
+    int selectBoardCountByCondition(SearchCondition searchCondition);
+
+    /**
+     * 단일 게시물 가져오기
+     *
+     * @param boardId pk
+     * @return 단일 게시물
+     */
+    BoardDto selectById(Long boardId);
 
     /**
      * board 추가
-     * @param boardDTO
+     *
+     * @param board 추가할 게시물
+     * @return 추가 후 생성된 pk
      */
-    int createBoard(BoardDto boardDTO);
+    int insertBoard(BoardDto board);
+
+    /**
+     * board 데이터 update
+     *
+     * @param board 수정할 게시물 데이터
+     */
+    void updateBoard(BoardDto board);
 
     /**
      * view 1 증가
-     * @param boardId
+     *
+     * @param boardId pk
      */
-    int updateView(Long boardId);
+    void updateView(Long boardId);
 
     /**
      * pk로 board 삭제
-     * @param boardId
+     *
+     * @param boardId pk
      */
-    int deleteById(Long boardId);
+    void deleteById(Long boardId);
 
     /**
-     * board update
-     * @param board
+     * pk와 입력한 비밀번호와 일치하는 게시물의 수 가져오기
+     *
+     * @param boardId         pk
+     * @param enteredPassword 입력한 pw
+     * @return 일치하는 게시물의 수
      */
-    int updateBoard(BoardDto board);
+    int selectByIdAndPassword(@Param("boardId") Long boardId, @Param("enteredPassword") String enteredPassword);
+
 }
+
+
