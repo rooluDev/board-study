ee78b330954a220d22023af847a1d1eea31c7ff9
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2025-05-18 23:31:38 +0900

    docs : 주석추가

--- study-spa/backend/src/main/java/com/study/service/FileService.java
+++ study-spa/backend/src/main/java/com/study/service/FileService.java
@@ -1,7 +1,6 @@
 package com.study.service;
 
 import com.study.dto.FileDto;
-import com.study.exception.FileNotFoundException;
 import com.study.mapper.FileMapper;
 import com.study.repository.LocalSaveFileRepository;
 import com.study.repository.SaveFileRepository;
@@ -14,7 +13,6 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.*;
 import java.util.List;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
 /**
  * 파일 관련 비지니스 로직
@@ -32,18 +30,24 @@ public class FileService {
         this.saveFileRepository = localSaveFileRepository;
     }
 
-    /**
-     * board와 관련된 file들 가져오기
-     * @param boardId
-     * @return
-     */
-    public List<FileDto> findFileListByBoardId(Long boardId){
-        // Entity -> Dto
-        List<FileDto> fileDtoList = fileMapper.findByBoardId(boardId).stream()
-                .map(entity -> entity.toFileDto())
-                .collect(Collectors.toList());
 
-        return fileDtoList;
+    /**
+     * 게시물에 저장된 파일들 가져오기
+     *
+     * @param boardId board Pk
+     * @return 게시물에 있는 천부파일 리스트
+     */
+    public List<FileDto> getFileListByBoardId(Long boardId) {
+        return fileMapper.selectByBoardId(boardId);
+    }
+    /**
+     * 단일 파일 찾기
+     *
+     * @param fileId pk
+     * @return 파일
+     */
+    public FileDto getFile(Long fileId) {
+        return fileMapper.selectById(fileId);
     }
 
     /**
@@ -52,12 +56,12 @@ public class FileService {
      * @param multipartFiles
      * @param boardId
      */
-    public void addFile(MultipartFile[] multipartFiles, Long boardId) throws IOException {
+    public void addFile(List<MultipartFile> multipartFiles, Long boardId) throws IOException {
         if(multipartFiles != null) {
             for (MultipartFile multipartFile : multipartFiles) {
                 if (!multipartFile.isEmpty()) {
                     // File DTO 생성
-                    com.study.entity.File file = com.study.entity.File.builder()
+                    FileDto file = FileDto.builder()
                             .boardId(boardId)
                             .originalName(multipartFile.getOriginalFilename())
                             .physicalName(UUID.randomUUID().toString())
@@ -67,37 +71,29 @@ public class FileService {
                             .build();
 
                     // Server 저장
-                    saveFileRepository.createFile(file.toFileDto(),multipartFile);
+                    saveFileRepository.createFile(file,multipartFile);
 
                     // File DB Add
-                    fileMapper.createFile(file);
+                    fileMapper.insertFile(file);
                 }
             }
         }
     }
-
     /**
-     * 게시물의 등록된 파일 DB에서 삭제
-     * @param boardId
+     * 게시물에 저장된 파일들 삭제
+     *
+     * @param boardId board Pk
      */
-    public int deleteFilesByBoardId(Long boardId){
-        return fileMapper.deleteByBoardId(boardId);
+    public void deleteFileListByBoardId(Long boardId) {
+        fileMapper.deleteByBoardId(boardId);
     }
 
     /**
-     * 하나의 파일 삭제
-     * @param fileId
+     * 단일 파일 삭제
+     *
+     * @param fileId Pk
      */
-    public int deleteFileByFileId(Long fileId){
-        return fileMapper.deleteById(fileId);
-    }
-
-    /**
-     * pk로 파일 정보 가져오기
-     * @param fileId
-     * @return
-     */
-    public FileDto findByFileId(Long fileId) throws FileNotFoundException {
-        return fileMapper.findByFileId(fileId).orElseThrow(() -> new com.study.exception.FileNotFoundException()).toFileDto();
+    public void deleteById(Long fileId) {
+        fileMapper.deleteById(fileId);
     }
 }
