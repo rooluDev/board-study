9be181ae33c8bdd7769f636f462dcf0bd8181fe5
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2025-05-18 22:34:22 +0900

    fix : delete findById

--- study-spa/backend/src/main/java/com/study/controller/CategoryController.java
+++ study-spa/backend/src/main/java/com/study/controller/CategoryController.java
@@ -1,16 +1,10 @@
 package com.study.controller;
 
-
 import com.study.dto.CategoryDto;
-import com.study.exception.common.success.ApiResponse;
-import com.study.exception.common.success.SuccessCode;
 import com.study.service.CategoryService;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,33 +15,22 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("/api")
-@Slf4j
+@RequiredArgsConstructor
 public class CategoryController {
-    private final CategoryService categoryService;
 
-    @Autowired
-    public CategoryController(CategoryService categoryService) {
-        this.categoryService = categoryService;
-    }
+    private final CategoryService categoryService;
 
     /**
      * 모든 카테고리 리스트 요청
      *
-     * @return
+     * @return {
+     * categoryList: []
+     * }
      */
     @GetMapping("/categories")
-    public ApiResponse<List<CategoryDto>> getCategoryList() {
-        return new ApiResponse(categoryService.getCategoryList(), SuccessCode.SELECT_SUCCESS);
-    }
+    public ResponseEntity<List<CategoryDto>> getCategoryList() {
+        List<CategoryDto> categoryList = categoryService.getCategoryList();
 
-    /**
-     * 단일 카테고리 요청
-     *
-     * @param categoryId
-     * @return
-     */
-    @GetMapping("/category/{categoryId}")
-    public ApiResponse<CategoryDto> getCategory(@PathVariable Long categoryId) throws Exception {
-        return new ApiResponse(categoryService.findById(categoryId),SuccessCode.SELECT_SUCCESS);
+        return ResponseEntity.ok().body(categoryList);
     }
 }
