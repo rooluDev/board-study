ce608a7bcabb975f7e211692a2f847ca5b2e9a26
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2025-05-18 23:31:59 +0900

    docs : 주석추가

--- study-spa/backend/src/main/java/com/study/repository/SaveFileRepository.java
+++ study-spa/backend/src/main/java/com/study/repository/SaveFileRepository.java
@@ -5,6 +5,17 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
+/**
+ * 파일의 메타데이터 저장하는 인터페이스
+ */
 public interface SaveFileRepository{
+
+    /**
+     * 파일 저장
+     *
+     * @param fileDto 파일 정보
+     * @param multipartFile multipartFile
+     * @throws IOException IOException
+     */
     void createFile(FileDto fileDto, MultipartFile multipartFile) throws IOException;
 }
