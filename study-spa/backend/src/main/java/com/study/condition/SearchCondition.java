08b66b177a9137f2027c05a17c6cd801f358028d
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2025-05-18 23:44:07 +0900

    feat : SearchCondition 추가

---
+++ study-spa/backend/src/main/java/com/study/condition/SearchCondition.java
@@ -0,0 +1,63 @@
+package com.study.condition;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * 검색 조건
+ */
+@Getter
+@Setter
+public class SearchCondition {
+    private String startDate;
+    private String endDate;
+    private int categoryId;
+    private String searchText;
+    private int pageNum;
+    private int startRow;
+    private int pageSize = 10;
+    private Timestamp startDateTimestamp;
+    private Timestamp endDateTimestamp;
+
+    /**
+     * 기본 검색 조건 생성자
+     */
+    public SearchCondition() {
+        this.startDate = LocalDate.now().minusYears(1).format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        this.endDate = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        this.categoryId = -1;
+        this.searchText = "";
+        this.pageNum = 1;
+    }
+
+    public Timestamp getStartDateTimestamp() {
+        LocalDate localStateDate = LocalDate.parse(this.startDate);
+        LocalDateTime startDate = localStateDate.atTime(LocalTime.MIN);
+        return Timestamp.valueOf(startDate);
+    }
+
+    public Timestamp getEndDateTimestamp() {
+        LocalDate localEndDate = LocalDate.parse(this.endDate);
+        LocalDateTime endDate = localEndDate.atTime(LocalTime.MAX);
+        return Timestamp.valueOf(endDate);
+    }
+
+    /**
+     * OFFSET Getter
+     *
+     * @return offset
+     */
+    public int getStartRow() {
+        if (pageNum == 1) {
+            return 0;
+        }
+        return (this.pageNum - 1) * this.pageSize;
+    }
+
+}
