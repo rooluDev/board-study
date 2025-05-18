61e3d227a414ea4bd6e5e7890f87d05bc3a5476c
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2025-05-18 22:34:36 +0900

    fix : delete findById

--- study-spa/backend/src/main/java/com/study/service/CategoryService.java
+++ study-spa/backend/src/main/java/com/study/service/CategoryService.java
@@ -1,45 +1,28 @@
 package com.study.service;
 
 import com.study.dto.CategoryDto;
-import com.study.exception.CategoryNotFoundException;
 import com.study.mapper.CategoryMapper;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.stream.Collectors;
+
 
 /**
- * 카테고리 관련 비지니스 로직
+ * 카테고리 서비스
  */
 @Service
+@RequiredArgsConstructor
 public class CategoryService {
+
     private final CategoryMapper categoryMapper;
 
-    @Autowired
-    public CategoryService(CategoryMapper categoryMapper){
-        this.categoryMapper = categoryMapper;
-    }
-
     /**
-     * DB에 있는 Category 전부 가져오기
-     * @return
+     * 카테고리 List 가져오기
+     *
+     * @return 카테고리 리스트
      */
     public List<CategoryDto> getCategoryList(){
-        // wrapper class
-        List<CategoryDto> categoryDtoList = categoryMapper.getCategoryList().stream()
-                .map(entity-> entity.toCategoryDto())
-                .collect(Collectors.toList());
-
-        return categoryDtoList;
-    }
-
-    /**
-     * pk로 Category 하나 가져오기
-     * @param categoryId
-     * @return
-     */
-    public CategoryDto findById(Long categoryId) throws CategoryNotFoundException{
-        return categoryMapper.findById(categoryId).orElseThrow(()-> new CategoryNotFoundException()).toCategoryDto();
+        return categoryMapper.getCategory();
     }
 }
