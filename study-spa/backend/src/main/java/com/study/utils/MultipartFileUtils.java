a1b876d05418d8c074910961876e900e44b8f526
rooluDev <100749004+Kim-seung-hyun98@users.noreply.github.com>
2025-06-02 01:47:40 +0900

    docs : 주석 추가

--- study-spa/backend/src/main/java/com/study/utils/MultipartFileUtils.java
+++ study-spa/backend/src/main/java/com/study/utils/MultipartFileUtils.java
@@ -7,10 +7,11 @@ public class MultipartFileUtils {
 
     /**
      * 이름에서 확장자 추출
-     * @param originalFileName
-     * @return
+     *
+     * @param originalFileName file full name
+     * @return extension
      */
-    public static String extractExtension(String originalFileName){
+    public static String extractExtension(String originalFileName) {
         if (originalFileName != null) {
             int lastDotIndex = originalFileName.lastIndexOf('.');
             if (lastDotIndex > 0) {
